de1facfa42f86c25f9f1a25ee0753890bf1e03b8
habib16247 <habib162470@gmail.com>
2024-01-02 01:59:48 +0600

    EmailPasswordAuthValidation

--- src/ContextAPI/ContextProvider.jsx
+++ src/ContextAPI/ContextProvider.jsx
@@ -2,11 +2,15 @@ import { createContext, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
 import { app } from "../FireBase/Firebase.config";
 import Swal from 'sweetalert2';  // Import Swal correctly
+// import { useNavigate } from "react-router-dom";
 
 export const AuthProvider = createContext(null);
 const auth = getAuth(app);
 
 const ContextProvider = ({ children }) => {
+
+    // const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -15,7 +19,7 @@ const ContextProvider = ({ children }) => {
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, password, confirmPassword } = e.target;
     setFormData((prevState) => ({
       ...prevState,
       [name]: value,
@@ -26,20 +30,49 @@ const ContextProvider = ({ children }) => {
     e.preventDefault();
     console.log(formData);
 
+    if(formData.username === "" || formData.email === "" || formData.password === "" || formData.confirmPassword === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "please fill the form",
+          });
+          return;
+    }
+
+    if (!/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/.test(formData.password)) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Password must be at least 8 characters, one uppercase letter, one lowercase letter, and one number",
+        });
+        return;
+      }
+
+      if (formData.password !== formData.confirmPassword) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+          footer: '<a href="#">Why do I have this issue?</a>',
+        });
+        return;
+      }
+
     createUserWithEmailAndPassword(auth, formData.email, formData.password)
       .then((res) => {
         const user = res.user;
         console.log(user);
-
+        updateProfile(auth.currentUser, {
+            displayName: formData.username,
+          })
+        //   .then(() => {
+        //     navigate("/")
+        //   })
         Swal.fire({
           title: "Success!",
           text: "Do you want to continue",
           icon: "success",
           confirmButtonText: "Cool",
-        });
-
-        updateProfile(auth.currentUser, {
-          displayName: formData.username,  // Corrected the form data field
         });
       })
       .catch((error) => {
@@ -61,3 +94,4 @@ const ContextProvider = ({ children }) => {
 };
 
 export default ContextProvider;
+   
